004541e95667363412b70d1229bb96fc6b4cc3cf
Nate Tuvera <ntuvera@gmail.com>
2019-11-16 18:49:30 -0500

    Add Feign Route for grabbing user object based on userId

--- users-api/src/main/java/com/example/usersapi/controller/UsersApiController.java
+++ users-api/src/main/java/com/example/usersapi/controller/UsersApiController.java
@@ -12,12 +12,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 public class UsersApiController {
     @Autowired
     UserServiceImpl userService;
-  
+
     @Autowired
     UserProfileServiceImpl userProfileService;
 
@@ -32,12 +33,12 @@ public class UsersApiController {
     public ResponseEntity<?> signUpUser(@RequestBody User newUser){
         return ResponseEntity.ok(userService.signUpUser(newUser));
     }
-  
+
     @PostMapping("/login")
     public ResponseEntity<?> loginUser(@RequestBody User user){
         return ResponseEntity.ok(userService.loginUser(user));
     }
-  
+
     @GetMapping("/list")
     public Iterable<User> listAllUsers(){
         return userService.listAll();
@@ -53,19 +54,27 @@ public class UsersApiController {
     public List<CommentBean> listCommentsByUser(@RequestHeader("userId") Integer userId) {
         return commentClient.getAllCommentsByUser();
     }
-      
+
     @GetMapping("/profile")
     public UserProfile getUserProfile(@RequestHeader("userId") String userId){
         return userProfileService.getUserProfile(Integer.parseInt(userId));
     }
-  
+
     @PostMapping("/profile")
     public UserProfile createUserProfile(@RequestBody UserProfile userProfile, @RequestHeader("userId") int userId){
         return userProfileService.createProfile(userProfile, userId);
     }
-  
+
     @DeleteMapping("/profile")
     public UserProfile deleteUserProfile(@RequestBody UserProfile userProfile, @RequestHeader("userId") int userId){
         return userProfileService.deleteProfile(userId);
+    }
+
+
+    // FeignClient Routes for inter-service communication
+
+    @GetMapping("/identify/{userId}")
+    public Optional<User> findUserById(@PathVariable int userId) {
+        return userService.findById(userId);
     }
 }
--- users-api/src/main/java/com/example/usersapi/service/UserService.java
+++ users-api/src/main/java/com/example/usersapi/service/UserService.java
@@ -3,8 +3,11 @@ package com.example.usersapi.service;
 import com.example.usersapi.model.JwtResponse;
 import com.example.usersapi.model.User;
 
+import java.util.Optional;
+
 public interface UserService {
     public JwtResponse signUpUser(User newUser);
     public JwtResponse loginUser(User user);
     public Iterable<User> listAll();
+    public Optional<User> findById(int userId);
 }
