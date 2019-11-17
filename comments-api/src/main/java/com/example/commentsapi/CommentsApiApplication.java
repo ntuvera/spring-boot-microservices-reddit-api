8068976c60d6aeb2dd412e47b4955abeb6ce9965
Nate Tuvera <ntuvera@gmail.com>
2019-11-17 02:39:48 -0500

    WIP: Zuul routing in progess: stuck on connection refused at /login -> /user/login

--- comments-api/src/main/java/com/example/commentsapi/CommentsApiApplication.java
+++ comments-api/src/main/java/com/example/commentsapi/CommentsApiApplication.java
@@ -2,12 +2,14 @@ package com.example.commentsapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @EnableEurekaClient
+@EnableDiscoveryClient
 @RestController
 public class CommentsApiApplication {
 
--- posts-api/src/main/java/com/example/postsapi/PostsApiApplication.java
+++ posts-api/src/main/java/com/example/postsapi/PostsApiApplication.java
@@ -2,6 +2,7 @@ package com.example.postsapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 @SpringBootApplication
 @EnableEurekaClient
 @EnableFeignClients
+@EnableDiscoveryClient
 @RestController
 public class PostsApiApplication {
 	@GetMapping("/")
