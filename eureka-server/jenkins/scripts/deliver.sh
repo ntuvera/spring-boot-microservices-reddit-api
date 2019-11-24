0478a8c33ed8195bc3fddbadf133ab594507aed9
Steven Huang <steven.charles.huang@gmail.com>
2019-11-23 20:47:35 -0500

    updated deliver.sh

--- eureka-server/jenkins/scripts/deliver.sh
+++ eureka-server/jenkins/scripts/deliver.sh
@@ -23,4 +23,5 @@ set +x
 echo 'The following command runs and outputs the execution of your Java'
 echo 'application (which Jenkins built using Maven) to the Jenkins UI.'
 set -x
-java -jar eureka-server/target/${NAME}-${VERSION}.jar
+pwd
+java -jar /target/${NAME}-${VERSION}.jar
